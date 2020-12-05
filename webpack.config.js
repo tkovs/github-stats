28d1bece383b5363bcc7ada87996e4a847a05f30
tkovs <joao.rodrigues@arapiraca.ufal.br>
2020-12-05 17:42:14 -0300

    Log webpack env

--- webpack.config.js
+++ webpack.config.js
@@ -1,37 +1,41 @@
-const path = require('path');
-const webpack = require('webpack');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const outputDir = path.join(__dirname, 'build/');
+const path = require("path");
+const webpack = require("webpack");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const outputDir = path.join(__dirname, "build/");
 
-const isProd = process.env.NODE_ENV === 'production';
+const isProd = process.env.NODE_ENV === "production";
 
-module.exports = (env) => ({
-  entry: './src/Index.bs.js',
-  mode: isProd ? 'production' : 'development',
-  output: {
-    path: outputDir,
-    filename: 'Index.js'
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-      favicon: "./favicon.ico",
-      template: 'index.html',
-      inject: false
-    }),
-    new webpack.DefinePlugin({ 'process.env.GITHUB_TOKEN': JSON.stringify(env.GITHUB_TOKEN) })
-  ],
-  devServer: {
-    compress: true,
-    contentBase: outputDir,
-    port: process.env.PORT || 8000,
-    historyApiFallback: true
-  },
-  module: {
-    rules: [
-      {
-        test: /\.css$/i,
-        use: ["style-loader", "css-loader"],
-      },
+module.exports = () => {
+  return {
+    entry: "./src/Index.bs.js",
+    mode: isProd ? "production" : "development",
+    output: {
+      path: outputDir,
+      filename: "Index.js",
+    },
+    plugins: [
+      new HtmlWebpackPlugin({
+        favicon: "./favicon.ico",
+        template: "index.html",
+        inject: false,
+      }),
+      new webpack.DefinePlugin({
+        "process.env.GITHUB_TOKEN": JSON.stringify(process.env.GITHUB_TOKEN),
+      }),
     ],
-  },
-});
+    devServer: {
+      compress: true,
+      contentBase: outputDir,
+      port: process.env.PORT || 8000,
+      historyApiFallback: true,
+    },
+    module: {
+      rules: [
+        {
+          test: /\.css$/i,
+          use: ["style-loader", "css-loader"],
+        },
+      ],
+    },
+  };
+};
